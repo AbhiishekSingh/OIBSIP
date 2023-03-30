9c62efcb27ec65e9cd78ca8d7d3be3fb660f9011
AbhiishekSingh <129388018+AbhiishekSingh@users.noreply.github.com>
2023-03-31 01:10:02 +0530

    Add files via upload

---
+++ Calculator/Calculator.js
@@ -0,0 +1,29 @@
+let  strings=document.getElementById("input");
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+
+      function del(){
+        strings.value=strings.value.slice(0,-1);
+      }
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+   
+    else if(e.target.innerHTML == 'C'){
+      string = " ";
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'DE'){
+      string = string.slice(0,-1); 
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
